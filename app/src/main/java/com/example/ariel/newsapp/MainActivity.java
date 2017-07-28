9dfedbd9731996972cce2ab98a98aecfd328aa2a
arielsl <asuarezlg@gmail.com>
2017-07-28 02:34:02 -0700

    App is working, need to check for updating news.

--- app/src/main/java/com/example/ariel/newsapp/MainActivity.java
+++ app/src/main/java/com/example/ariel/newsapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.ariel.newsapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,88 +18,139 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-import com.example.ariel.newsapp.utilities.NetworkUtils;
-import com.example.ariel.newsapp.utilities.JsonUtil;
-import com.example.ariel.newsapp.models.NewsItem;
+import com.example.ariel.newsapp.dbhelper.Contract;
+import com.example.ariel.newsapp.dbhelper.DBHelper;
+import com.example.ariel.newsapp.dbhelper.DatabaseUtils;
+import com.example.ariel.newsapp.tasks.FetchNewstask;
+import com.example.ariel.newsapp.utilities.SchedulerUtils;
 
-import org.json.JSONException;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Void>, ViewAdapter.NewsClickListener{
 
-public class MainActivity extends AppCompatActivity {
-
-    static final String TAG = "mainactivity";
+    static final String TAG = "MAIN_ACTIVITY";
     private RecyclerView recyclerView;
     private ViewAdapter startAdapter;
+    private static final int NEWS_LOADER = 1;
+
+    //These new variables will help us read the database for its data
+    private Cursor cursor;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "CREATED ACTIVITY AND TOOK LAYOUT");
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(startAdapter);
-        new FetchNewsTask().execute();
+
+        //From the shared preferences, check if the app has run in the phone before, if it has not,
+        //run the loader to and mark it as already ran
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean firstRun = sp.getBoolean("firstRun", true);
+        if(firstRun){
+            load();
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("firsRun", false);
+            editor.apply();
+        }
+        Log.v(TAG, "CHECKED PREFERENCES AND RESULTED IN FIRST RUN = " + firstRun);
+
+        //Call the job dispatcher to run the refresh method with the given intervals
+        SchedulerUtils.scheduleRefresh(this);
     }
 
-
-    public class FetchNewsTask extends AsyncTask<String, Void, ArrayList<NewsItem>>{
-        @Override
-        protected ArrayList<NewsItem> doInBackground(String... params){
-            ArrayList<NewsItem> articleList = null;
-
-            URL newsRequestUrl = NetworkUtils.mUriBuilder();
-
-            try{
-                String jsonNewsDataResponse = NetworkUtils.getResponseFromHttpUrl(newsRequestUrl);
-                articleList = JsonUtil.parseJSON(jsonNewsDataResponse);
-
-            } catch (IOException e){
-                e.printStackTrace();
-            } catch(JSONException e){
-                e.printStackTrace();
-            }
-
-            return articleList;
-        }
-
-        @Override
-        protected void onPostExecute(final ArrayList<NewsItem> articleList){
-            super.onPostExecute(articleList);
-
-            if(articleList!= null) {
-                ViewAdapter adapter = new ViewAdapter(articleList, new ViewAdapter.NewsClickListener() {
-                    @Override
-                    public void onNewsClick(int clickedItemIndex) {
-                        String url = articleList.get(clickedItemIndex).getUrl();
-                        Log.d(TAG, String.format("URL CLICKED: %s", url));
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                        startActivity(intent);
-                    }
-                });
-                recyclerView.setAdapter(adapter);
-            }
-
-        }
-    }
-
-    //Refresh button idea from https://stackoverflow.com/questions/27822002/actionbar-refresh-button
+    //When the activity starts, get all the news info from the database and pass it to the adapter
+    //to create each news view
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    protected void onStart(){
+        super.onStart();
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+        Log.v(TAG, "GOT NEWS FROM DATABASE, SIZE = " + cursor.getCount());
+        startAdapter = new ViewAdapter(cursor, this);
+        recyclerView.setAdapter(startAdapter);
+    }
+
+    //When the activity stops make sure to close the database connection
+    @Override
+    protected void onStop(){
+        super.onStop();
+        db.close();
+        cursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    //When the refresh button is pressed, reread the database
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.refresh) {
-            new FetchNewsTask().execute();
+            load();
             return true;
-        }else{
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //We change the Async to be a loader
+    public Loader<Void> onCreateLoader(int id, final Bundle args){
+        return new AsyncTaskLoader<Void>(this) {
+
+            @Override
+            public void onStartLoading(){
+                super.onStartLoading();
+            }
+
+            //On the background thread, use the fetch news article method to retrieve the info
+            //of the articles from the API
+            @Override
+            public Void loadInBackground() {
+                FetchNewstask.fetchNewsToDatabase(MainActivity.this);
+                return null;
+            }
+        };
+    }
+
+    //When the loader has finished, read the database and pass the cursor to the adapter to
+    //populate the view
+    @Override
+    public void onLoadFinished(Loader<Void> loader, Void data){
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+
+        startAdapter = new ViewAdapter(cursor, this);
+        recyclerView.setAdapter(startAdapter);
+        startAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Void> data){
+
+    }
+
+    //When an article is clicked this method will be called, we get the position of the item
+    //and we pass it to the cursor to read that position in the database.
+    //We get the link of the article from the database and we start an intent with the url
+    @Override
+    public void onNewsClick(Cursor cursor, int clickedItem){
+        cursor.moveToPosition(clickedItem);
+        String url = cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_URL));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    //Reload when called
+    public void load(){
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.restartLoader(NEWS_LOADER, null, this).forceLoad();
     }
 }
--- app/src/main/java/com/example/ariel/newsapp/ViewAdapter.java
+++ app/src/main/java/com/example/ariel/newsapp/ViewAdapter.java
@@ -13,24 +13,36 @@ import android.widget.TextView;
 import com.example.ariel.newsapp.models.NewsItem;
 import java.util.ArrayList;
 
+//All the new imports we will need to properly get the news info
+import com.example.ariel.newsapp.dbhelper.Contract;
+import com.squareup.picasso.Picasso;
+import android.database.Cursor;
+import android.widget.ImageView;
+
 
 public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.NewsHolder>{
     private ArrayList<NewsItem> articlesList;
     NewsClickListener listener;
+    //We need a context to change the view with the image and a cursor to get the info
+    //from the database
+    private Context context;
+    Cursor cursor;
 
-    public ViewAdapter(ArrayList<NewsItem> articlesList, NewsClickListener listener){
-        this.articlesList = articlesList;
+    //Now the constructor will take the cursor to the database to be able to read the news data
+    public ViewAdapter(Cursor cursor, NewsClickListener listener){
+        this.cursor = cursor;
         this.listener = listener;
 
     }
 
+    //We change the interface to include the cursor to the database to read
     public interface NewsClickListener{
-        void onNewsClick(int clickedNewsIndex);
+        void onNewsClick(Cursor cursor, int clickedNewsIndex);
     }
 
     @Override
     public NewsHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
-        Context context = viewGroup.getContext();
+        context = viewGroup.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         boolean attachToParentImmediately = false;
         View view = inflater.inflate(R.layout.article, viewGroup, attachToParentImmediately);
@@ -44,9 +56,11 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.NewsHolder>{
         newsHolder.bind(position);
     }
 
+    //We change the count to be the number of elements in the database cursor pointing to the news
+    //articles
     @Override
     public int getItemCount(){
-        return articlesList == null ? 0 : articlesList.size();
+        return cursor.getCount();
     }
 
 
@@ -54,6 +68,8 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.NewsHolder>{
         TextView mTitleText;
         TextView mDescriptionText;
         TextView mTimeText;
+        //This image variable will draw the news image using picasso
+        ImageView mImage;
 
         public NewsHolder(View view){
             super(view);
@@ -62,21 +78,37 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.NewsHolder>{
             mTimeText = (TextView) view.findViewById(R.id.news_time);
             mDescriptionText = (TextView) view.findViewById(R.id.news_description);
 
+            //We get the reference to the image to be able to upload it to the view
+            mImage = (ImageView) view.findViewById(R.id.news_image);
+
             view.setOnClickListener(this);
         }
 
+        //We change the bind method to get the items in the cursor instead of an array
         public void bind(int position){
-            NewsItem news = articlesList.get(position);
+            //Get the element in the position according to the variable
+            cursor.moveToPosition(position);
 
-            mTitleText.setText(news.getTitle());
-            mDescriptionText.setText(news.getDescription());
-            mTimeText.setText(news.getDate());
+            //Use the references to the elements in the view to set their values to the values
+            //in the database for the respective news article
+            mTitleText.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_TITLE)));
+            mDescriptionText.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_DESCRIPTION)));
+            mTimeText.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_DATE)));
+
+            //Get the url of the image in the database
+            String imageUrl = cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_IMGURL));
+
+            //If we succesfully get the url of the image, use picasso to upload the image to the view
+            if (imageUrl != null){
+                Picasso.with(context).load(imageUrl).into(mImage);
+            }
         }
 
+        //As above, when an item is clicked we pass the cursor to the database
         @Override
         public void onClick(View view){
             int pos = getAdapterPosition();
-            listener.onNewsClick(pos);
+            listener.onNewsClick(cursor, pos);
         }
     }
 }
